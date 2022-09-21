58314e513e9857e8e454819cd4ea94aa8d094852
Ed Beard <ed.r.beard@gmail.com>
2022-09-21 08:55:57 -0400

    Prompts

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-
-const profileDataArgs = process.argv.slice(2);
-
-const [name, github] = profileDataArgs;
+const inquirer = require('inquirer');
+// const fs = require('fs');
+//const generatePage = require('./src/page-template.js');
 
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
+//const [name, github] = profileDataArgs;
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+
+//fs.writeFile('./index.html', generatePage(name, github), err => {
+//  if (err) throw new Error(err);
+
+//  console.log('Portfolio complete! Check out index.html to see the output!');
+//});
+inquirer
+  .prompt ([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers))
